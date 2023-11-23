fdf9276568b3fd1411105b779d0498a9897a0e8c
Shannqa <shannqa@gmail.com>
2023-11-23 20:28:11 +0100

    Drag and dropping works for more than 2-cell ships!

--- src/dom.js
+++ src/dom.js
@@ -2,10 +2,6 @@ import { Gameboard, Player } from "./game.js";
 
 // icons https://www.freepik.com/free-vector/set-silhouettes-naval-ships_11052928.htm#query=warship&position=2&from_view=keyword&track=sph&uuid=fe968b55-ef3b-481a-b69a-ecbbb60a471c
 
-
-
-
-
 function createDom() {
   const body = document.querySelector("body");
   const header = document.createElement("div");
@@ -15,19 +11,19 @@ function createDom() {
   header.classList.add("header");
   const title = document.createElement("div");
   title.textContent = "Battleships";
-  
+
+  main.classList.add("main");
   const enemyBoard = document.createElement("div");
   const enemyBoardTitle = document.createElement("div");
   enemyBoardTitle.textContent = "Enemy board";
-  
+
   const ownBoard = document.createElement("div");
   const ownBoardTitle = document.createElement("div");
   ownBoardTitle.textContent = "Your board";
-  
+
   const ships = document.createElement("div");
   const shipsTitle = document.createElement("div");
   shipsTitle.textContent = "Place your ships";
-  
 
   header.appendChild(title);
   main.appendChild(enemyBoard);
@@ -40,8 +36,6 @@ function createDom() {
   body.appendChild(header);
   body.appendChild(main);
   body.appendChild(footer);
-  
 }
 
-
-export { createDom }
+export { createDom };
--- src/index.js
+++ src/index.js
@@ -4,9 +4,71 @@ import imgsrc from "./img_logo.gif";
 import css from "./style.css";
 
 createDom();
-playGame();
+// playGame();
 
 const body = document.querySelector("body");
+const main = document.querySelector(".main");
+
+function createGrid() {
+  let gridArray = [];
+  for (let x = 0; x < 10; x++) {
+    gridArray.push([]);
+    for (let y = 0; y < 10; y++) {
+      gridArray[x].push(null);
+    }
+  }
+  return gridArray;
+}
+
+function drawGrid() {
+  const array = createGrid();
+
+  const body = document.querySelector("body");
+  const grid = document.createElement("div");
+
+  array.forEach((row, rindex) => {
+    row.forEach((cell, cindex) => {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.setAttribute("id", `r${rindex}c${cindex}`);
+      if (cell === "ship") {
+        square.classList.add("ship");
+      }
+      if (cell === "miss") {
+        square.classList.add("miss");
+      }
+      if (cell === "hit") {
+        square.classList.add("hit");
+      }
+      square.addEventListener("drop", drop);
+      square.addEventListener("dragover", allowDrop);
+      grid.appendChild(square);
+    });
+  });
+
+  grid.classList.add("grid");
+  body.appendChild(grid);
+}
+
+drawGrid();
+
+const ship1 = document.createElement("div");
+const shipSq = document.createElement("div");
+const shipSq1 = document.createElement("div");
+shipSq.classList.add("square");
+shipSq1.classList.add("square");
+shipSq.classList.add("shipsq");
+shipSq1.classList.add("shipsq");
+ship1.classList.add("ship");
+body.appendChild(ship1);
+ship1.appendChild(shipSq);
+ship1.appendChild(shipSq1);
+
+ship1.setAttribute("draggable", "true");
+ship1.setAttribute("id", "ship1");
+ship1.addEventListener("dragstart", drag);
+
+/////////// tests
 const div1 = document.createElement("div");
 const img = new Image();
 img.src = imgsrc;
@@ -32,11 +94,5 @@ img.setAttribute("id", "drag1");
 img.setAttribute("draggable", "true");
 img.addEventListener("dragstart", drag);
 
-/*
-<div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
-<br>
-<img id="drag1" src="img_logo.gif" draggable="true" ondragstart="drag(event)" width="336" height="69"></img>
-
-*/
 body.appendChild(div1);
 body.appendChild(img);
