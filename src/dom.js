f963e549214f8abe5fb4d6b0a29b181db0be01d2
Shannqa <shannqa@gmail.com>
2023-11-24 22:56:35 +0100

    Clean up the code

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,61 @@
 import { Gameboard, Player } from "./game.js";
 
-// icons https://www.freepik.com/free-vector/set-silhouettes-naval-ships_11052928.htm#query=warship&position=2&from_view=keyword&track=sph&uuid=fe968b55-ef3b-481a-b69a-ecbbb60a471c
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  document.getElementById(data).classList.add("ship-on-board");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+function createGrid() {
+  let gridArray = [];
+  for (let x = 0; x < 10; x++) {
+    gridArray.push([]);
+    for (let y = 0; y < 10; y++) {
+      gridArray[x].push(null);
+    }
+  }
+  return gridArray;
+}
+
+function drawGrid() {
+  const ownBoard = document.querySelector(".own-board");
+  const array = createGrid();
+
+  const body = document.querySelector("body");
+  const grid = document.createElement("div");
+
+  array.forEach((row, rindex) => {
+    row.forEach((cell, cindex) => {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.setAttribute("id", `r${rindex}c${cindex}`);
+      if (cell === "ship") {
+        square.classList.add("ship");
+      }
+      if (cell === "miss") {
+        square.classList.add("miss");
+      }
+      if (cell === "hit") {
+        square.classList.add("hit");
+      }
+      square.addEventListener("drop", drop);
+      square.addEventListener("dragover", allowDrop);
+      grid.appendChild(square);
+    });
+  });
+
+  grid.classList.add("grid");
+  ownBoard.appendChild(grid);
+}
 
 function createDom() {
   const body = document.querySelector("body");
@@ -17,6 +72,17 @@ function createDom() {
   // const enemyBoardTitle = document.createElement("div");
   // enemyBoardTitle.textContent = "Enemy board";
 
+  header.appendChild(title);
+  // main.appendChild(enemyBoard);
+  // enemyBoard.appendChild(enemyBoardTitle);
+
+  body.appendChild(header);
+  body.appendChild(main);
+  body.appendChild(footer);
+}
+
+function prepareShips() {
+  const main = document.querySelector(".main");
   const ownBoard = document.createElement("div");
   const ownBoardTitle = document.createElement("span");
   ownBoard.classList.add("own-board");
@@ -27,16 +93,38 @@ function createDom() {
   const shipsTitle = document.createElement("span");
   shipsTitle.textContent = "Place your ships";
 
-  header.appendChild(title);
-  // main.appendChild(enemyBoard);
-  // enemyBoard.appendChild(enemyBoardTitle);
+  const shipList = document.createElement("div");
+  const shipLengths = [2, 3, 4, 5];
+  shipLengths.forEach((item, index) => {
+    const shipDiv = document.createElement("div");
+    shipDiv.classList.add("ship-div");
+    for (let i = 0; i < item; i++) {
+      const shipSq = document.createElement("div");
+      // shipSq.setAttribute("id", `sq0-to-place-${index}`);
+      if (i === 0) {
+        shipSq.classList.add("sq0-to-place");
+      }
+
+      shipSq.classList.add("square");
+      shipSq.classList.add("shipsq");
+
+      shipDiv.appendChild(shipSq);
+    }
+    shipDiv.setAttribute("draggable", "true");
+    shipDiv.setAttribute("id", `to-place-${index}`);
+
+    // need to handle error - when the item is dragged in the middle of two squares
+    shipDiv.addEventListener("dragstart", drag);
+    shipList.appendChild(shipDiv);
+  });
+
   main.appendChild(ownBoard);
   ownBoard.appendChild(ownBoardTitle);
   main.appendChild(placeShips);
   placeShips.appendChild(shipsTitle);
-  body.appendChild(header);
-  body.appendChild(main);
-  body.appendChild(footer);
+  placeShips.appendChild(shipList);
+
+  drawGrid();
 }
 
-export { createDom };
+export { createDom, prepareShips };
