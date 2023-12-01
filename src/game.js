a43e7d41ba86b86c909fff3f21cc2bf97be84aad
Shannqa <shannqa@gmail.com>
2023-12-01 23:37:14 +0100

    Make the AI smarter - try attaking spots next to one that's been hit in order to sink a ship

--- src/game.js
+++ src/game.js
@@ -36,6 +36,8 @@ class Gameboard {
     this.shipsList = [];
     this.receivedHits = 0;
     this.lostGame = false;
+    this.AIqueue = [];
+    this.AIhits = [];
   }
   static shipLengths = [2, 3, 4, 5];
 
@@ -59,41 +61,83 @@ class Gameboard {
     this.grid[coordsEnd[0]][coordsEnd[1]] = id;
   }
 
+  // need to remove coords from AIhits once the ship is sunk for sure
+
   receiveAttack(coords) {
     let square;
     let id = this.grid[coords[0]][coords[1]];
-    // console.log(id);
-    if (id === "miss" || id === "hit") {
-      // try again if invalid move
-      console.log("invalid move");
-      return "invalid move";
-    }
+    let [x, y] = coords;
 
     if (players.current === "human") {
       square = document.querySelector(
         `.enemy-square#r${coords[0]}c${coords[1]}`
       );
-      players.current = "AI";
+      if (id === "miss" || id === "hit") {
+        console.log("invalid move");
+        return "invalid move";
+      }
+      if (id === null) {
+        this.grid[coords[0]][coords[1]] = "miss";
+        square.classList.add("miss");
+      } else {
+        square.classList.add("hit");
+        let hitShip = this.shipsList[id];
+        this.grid[coords[0]][coords[1]] = "hit";
+        hitShip.hit();
+        this.receivedHits += 1;
+        this.checkIfLost();
+      }
     } else if (players.current === "AI") {
       square = document.querySelector(`.own-square#r${coords[0]}c${coords[1]}`);
-      players.current = "human";
-    }
-
-    //record a hit or miss
-    if (id === null) {
-      this.grid[coords[0]][coords[1]] = "miss";
-      square.classList.add("miss");
-    } else {
-      square.classList.add("hit");
-      let hitShip = this.shipsList[id];
-      this.grid[coords[0]][coords[1]] = "hit";
-      hitShip.hit();
-      this.receivedHits += 1;
-      this.checkIfLost();
+      if (id === null) {
+        this.grid[coords[0]][coords[1]] = "miss";
+        square.classList.add("miss");
+      } else if (id === "hit" || id === "miss") {
+        // invalid move - already attacked, so try again. it happens because an already attacked square can be added to the queue
+        return this.AIattack();
+      } else {
+        square.classList.add("hit");
+        let hitShip = this.shipsList[id];
+        this.grid[coords[0]][coords[1]] = "hit";
+        hitShip.hit();
+        this.receivedHits += 1;
+        this.checkIfLost();
+        this.AIhits.push(coords);
+        if (this.AIhits.length === 1) {
+          // add four sides of the current hit
+          this.AIqueue.push([x - 1, y]);
+          this.AIqueue.push([x + 1, y]);
+          this.AIqueue.push([x, y - 1]);
+          this.AIqueue.push([x, y + 1]);
+        } else {
+          // check if the adjacency is vertical or horizontal to the previous hit
+          // x,y -> current, i, j -> previous
+          let [i, j] = this.AIhits[this.AIhits.length - 1];
+          if (x === i) {
+            // row is the same
+            this.AIqueue.push([x, y - 1]);
+            this.AIqueue.push([x, y + 1]);
+            this.AIqueue.filter((item) => item[0] === x);
+          } else if (y === j) {
+            // column is the same
+            this.AIqueue.push([x - 1, y]);
+            this.AIqueue.push([x + 1, y]);
+            this.AIqueue.filter((item) => item[1] === y);
+          } else {
+            // no adjacency
+          }
+        }
+      }
     }
 
     console.log("owner: " + this.owner, "hits: " + this.receivedHits);
-    //console.log(this.grid);
+
+    if (players.current === "human") {
+      players.current = "AI";
+    } else if (players.current === "AI") {
+      players.current = "human";
+    }
+
     //if it's AI's turn now, send an attack
     if (players.current === "AI") {
       players.human.board.AIattack();
@@ -108,16 +152,34 @@ class Gameboard {
     }
     return;
   }
+  // when there's 5 adjacent hits, reset the queue, cause max ship is 5
+  // need to keep in mind already sunk ships. when a ship of a certain length is for sure sunk, the queue might be reset once this length is reached. e.g.
+  // ship 5 and 4 sunk > for sure might reset the queue and hits of ships of that length
 
+  // randomize the queue hits - now its always north south west east
+  // random attack should be a bit better
   AIattack() {
+    if (this.AIqueue.length === 0) {
+      // test - reseting the hits array when the queue is empty
+      this.AIhits = [];
+      this.AIrandomAttack();
+    } else {
+      let [x, y] = this.AIqueue.shift();
+      return this.receiveAttack([x, y]);
+    }
+  }
+
+  AIrandomAttack() {
     const x = Math.floor(Math.random() * 9);
     const y = Math.floor(Math.random() * 9);
     if (this.grid[x][y] === "hit" || this.grid[x][y] === "miss") {
-      return this.AIattack();
+      return this.AIrandomAttack();
     } else {
       return this.receiveAttack([x, y]);
     }
   }
+
+  AIcheckRandom() {}
 
   playerAttack(coords) {
     // if it's not the player's turn, clicking on enemy board will do nothing
